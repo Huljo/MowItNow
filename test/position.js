d98694f302fd274cb60d4a45c6ccc8560c5dcbbb
elhulu <drackar@voila.fr>
2016-06-03 18:53:04 +0200

    Add getCellOccupation method for prototype Grid Rename updatePosition to setItemPosition in prototype Grid Improve isCardinal method in prototype Position Add tests for prototype Position

---
+++ test/position.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var expect = require('expect.js');
+
+var Position = require('../lib/position');
+
+describe('Position', function() {
+    it('should throw an error when X is not valid', function () {
+        expect(function() {
+            new Position({x:false,y:1}, 'N');
+        }).to.throwException(/Coordinates are not valid/);
+    });
+    it('should throw an error when Y is not valid', function () {
+        expect(function() {
+            new Position({x:1,y:false}, 'N');
+        }).to.throwException(/Coordinates are not valid/);
+    });
+    it('should throw an error when C is not valid', function () {
+        expect(function() {
+            new Position({x:1,y:1}, false);
+        }).to.throwException(/Cardinal point is not valid/);
+    });
+});
